e8c3d463598f2a99a22dbd3d70b5cbb769db8589
lesajayaweera <lesanduluthmeen8@gmail.com>
2025-06-05 08:27:11 +0530

    made the bottom navigation

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -120,6 +120,19 @@ class Home extends StatelessWidget {
           ],
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.car_rental),
+            label: 'Products',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_3_rounded),
+            label: 'Products',
+          ),
+        ],
+      ),
     );
   }
 }
