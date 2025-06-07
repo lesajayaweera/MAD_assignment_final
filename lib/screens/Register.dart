d22ab867f861ce6e790bd2868d94328f80904bef
lesajayaweera <lesanduluthmeen8@gmail.com>
2025-06-07 15:30:19 +0530

    converted the Register.dart to a Stateful widget from a Stateless widget

--- lib/screens/Register.dart
+++ lib/screens/Register.dart
@@ -1,150 +1,163 @@
 import 'package:flutter/material.dart';
 
-class Register extends StatelessWidget {
+class Register extends StatefulWidget {
   const Register({super.key});
 
   @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-  body: SafeArea(
-    child: SingleChildScrollView(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, // Changed to 'center'
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 40), // Top padding
-            CircleAvatar(
-              radius: 70,
-              backgroundImage: AssetImage('asset/image/logo.png'),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Sign Up',
-              style: TextStyle(fontSize: 32,fontFamily: 'poppins', fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Find Your Dream Car',
-              style: TextStyle(fontSize: 24,fontFamily: 'poppins', color: Colors.grey.shade600, ),
-            ),
-            SizedBox(height: 40),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: TextField(
-                
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.person),
-                  labelText: 'Name',
-                  border: OutlineInputBorder(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // Changed to 'center'
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 40), // Top padding
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: AssetImage('asset/image/logo.png'),
                 ),
-              ),
-              
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: TextField(
-                
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.alternate_email_rounded),
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
+                SizedBox(height: 20),
+                Text(
+                  'Sign Up',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontFamily: 'poppins',
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: TextField(
-                
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.phone),
-                  labelText: 'Phone Number',
-                  border: OutlineInputBorder(),
+                SizedBox(height: 10),
+                Text(
+                  'Find Your Dream Car',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontFamily: 'poppins',
+                    color: Colors.grey.shade600,
+                  ),
                 ),
-              ),
-              
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: TextField(
-                obscureText: true,
-
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.lock),
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
-                  
-                ),
-              ),
-
-            ),
-
-            SizedBox(height: 20),
-            TextButton(
-              onPressed: () {
-                // Handle forgot password action
-              },
-              child: Text(
-                'Forgot Password?',
-                style: TextStyle(fontSize: 16, fontFamily: 'poppins', color: Colors.black87),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: SizedBox(
-                width: double.infinity,
-                child: TextButton(
-                  
-                  
-                  onPressed: () {
-                    // Handle login action
-                  },
-                  style: TextButton.styleFrom(
-                    
-                    backgroundColor: Colors.black,
-                    foregroundColor: Colors.white, // Button color
-                    minimumSize: Size(200, 50), // Set button width and height
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10), // Rounded corners
+                SizedBox(height: 40),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.person),
+                      labelText: 'Name',
+                      border: OutlineInputBorder(),
                     ),
                   ),
-                  child: Text('Sign Up', style: TextStyle(fontSize: 20,fontFamily: 'poppins')),
+                ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.alternate_email_rounded),
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.phone),
+                      labelText: 'Phone Number',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: TextField(
+                    obscureText: true,
 
-                  
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.lock),
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
                 ),
-                
-              ),
-            ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Don\'t have an account? ',
-                  style: TextStyle(fontSize: 16, fontFamily: 'poppins'),
-                ),
+
+                SizedBox(height: 20),
                 TextButton(
                   onPressed: () {
-                    // Handle sign up action
+                    // Handle forgot password action
                   },
                   child: Text(
-                    'Sign Up',
-                    style: TextStyle(fontSize: 16, fontFamily: 'poppins', color: Colors.blue),
+                    'Forgot Password?',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'poppins',
+                      color: Colors.black87,
+                    ),
                   ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: TextButton(
+                      onPressed: () {
+                        // Handle login action
+                      },
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        foregroundColor: Colors.white,
+                        // Button color
+                        minimumSize: Size(200, 50),
+                        // Set button width and height
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                            10,
+                          ), // Rounded corners
+                        ),
+                      ),
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(fontSize: 20, fontFamily: 'poppins'),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Don\'t have an account? ',
+                      style: TextStyle(fontSize: 16, fontFamily: 'poppins'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        // Handle sign up action
+                      },
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: 'poppins',
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-            
-          ],
+          ),
         ),
       ),
-    ),
-  ),
-);
-
+    );
   }
 }
